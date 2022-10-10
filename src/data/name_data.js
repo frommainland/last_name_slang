dcd83ff7831db176f9f9986941e2b3e6c2ed0c7e
frommainland <caihehuang@gmail.com>
2022-10-10 16:58:11 +0800

    add name content to name_data.js

--- src/data/name_data.js
+++ src/data/name_data.js
@@ -12,35 +12,35 @@ export const names = [
         id: 2,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "王",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "兽中之王",
+        englishName: 'Whitehead',
+        method: '联想',
         slang: '虎头'
     },
     {
         id: 3,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "钱",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
-        slang: '雪花'
+        description: "黄纸、白纸,剪成铜钱的形状焚烧给死者",
+        englishName: 'Quinones',
+        method: '联想',
+        slang: '花纸'
     },
     {
         id: 4,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "白",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "雪花白",
+        englishName: 'Becerra',
+        method: '联想',
         slang: '雪花'
     },
     {
         id: 5,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "冯",
-        description: "照谐音",
-        englishName: 'Zimmerman',
+        description: "缝补丁",
+        englishName: 'Fischer',
         method: '谐音',
         slang: '补丁'
     },
@@ -48,62 +48,62 @@ export const names = [
         id: 6,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "佫",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "一个人",
+        englishName: 'Hollis',
+        method: '拆字',
         slang: '单人'
     },
     {
         id: 7,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "孙",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "龙子龙孙",
+        englishName: 'Simpson',
+        method: '补充',
         slang: '龙子龙'
     },
     {
         id: 8,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "李",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "一脚门里，一脚门外",
+        englishName: 'Livingston',
+        method: '补充',
         slang: '一脚门'
     },
     {
         id: 9,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "陈",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "“千金”谐音“千斤”,指代“沉”;又以“沉”,谐音姓氏“陈”。",
+        englishName: 'Christiansen',
+        method: '联想',
         slang: '千斤子'
     },
     {
         id: 10,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "魏",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "撑”联想到“饱”；再由“饱”联想到“喂”，喂谐音",
+        englishName: 'Westwood',
+        method: '联想',
         slang: '撑肚子'
     },
     {
         id: 11,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "韩",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "“西北风”往往是寒气逼人，因用为姓氏“冷”之隐语。",
+        englishName: 'Hanson',
+        method: '联想',
         slang: '西北风'
     },
     {
         id: 12,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "杨",
-        description: "照谐音",
-        englishName: 'Zimmerman',
+        description: "因“羊”称为“山头子”",
+        englishName: 'Yanowitz',
         method: '谐音',
         slang: '山头子'
     },
@@ -111,19 +111,316 @@ export const names = [
         id: 13,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "朱",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "“哼哼”是朱（猪）的拟声词",
+        englishName: 'Zhamkochyan',
+        method: '联想',
         slang: '哼哼'
     },
     {
         id: 14,
         url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
         chineseName: "吕",
-        description: "照谐音",
-        englishName: 'Zimmerman',
-        method: '谐音',
+        description: "照“吕”字可析作两个“口”",
+        englishName: 'Lucero',
+        method: '拆字',
         slang: '双口蔓'
+    },
+    {
+        id: 15,
+        url: "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+        chineseName: "施",
+        description: "蚕吐丝，丝施谐音",
+        englishName: 'Sheehan',
+        method: '谐音',
+        slang: '蚕吐蔓'
+    },
+    {
+        id: 16,
+        slang: '跟头蔓',
+        chineseName: "张",
+        description: "在方言中，跌跟头又被称作“张跟头”",
+        method: '方言',
+        englishName: 'Zhanay',
+        url: ""
+    },
+    {
+        id: 17,
+        slang: '章圣贤',
+        chineseName: "孔",
+        description: "孔圣贤",
+        method: '联想',
+        englishName: 'Kovacs',
+        url: ""
+    },
+    {
+        id: 18,
+        slang: '地下湿',
+        chineseName: "曹",
+        description: "地下潮湿，潮曹谐音",
+        method: '谐音',
+        englishName: 'Callahan',
+        url: ""
+    },
+    {
+        id: 19,
+        slang: '海沙子',
+        chineseName: "阎",
+        description: "盐，出自于海水，因以“海水”指代“盐”。",
+        method: '联想',
+        englishName: 'Yarris',
+        url: ""
+    },
+    {
+        id: 20,
+        slang: '庚辛蔓',
+        chineseName: "金",
+        description: "庚辛属金",
+        method: '八卦',
+        englishName: 'Jinkens',
+        url: ""
+    },
+    {
+        id: 21,
+        slang: '仙人摘',
+        chineseName: "陶",
+        description: "仙人摘桃，桃陶谐音",
+        method: '补充',
+        englishName: 'Talley',
+        url: ""
+    },
+    {
+        id: 22,
+        slang: '大沟子',
+        chineseName: "江",
+        description: "用“大沟”比喻“江”河；由江河，联想到姓氏“江”。",
+        method: '联想',
+        englishName: 'Jiles',
+        url: ""
+    },
+    {
+        id: 23,
+        slang: '空中飘',
+        chineseName: "齐",
+        description: "空中飘旗，旗齐谐音",
+        method: '联想',
+        englishName: 'Quintanilla',
+        url: ""
+    },
+    {
+        id: 24,
+        slang: '龙争虎',
+        chineseName: "窦",
+        description: "龙争虎斗，斗窦谐音",
+        method: '补充',
+        englishName: 'Dowling',
+        url: ""
+    },
+    {
+        id: 25,
+        slang: '顶水蔓',
+        chineseName: "于",
+        description: "顶水（浪）而上者就是“鱼”",
+        method: '联想',
+        englishName: 'Yusupov',
+        url: ""
+    },
+    {
+        id: 26,
+        slang: '甜头子',
+        chineseName: "唐",
+        description: "甜莫过于糖。",
+        method: '联想',
+        englishName: 'Tanner',
+        url: ""
+    },
+    {
+        id: 27,
+        slang: '天下响',
+        chineseName: "雷",
+        description: "打雷",
+        method: '联想',
+        englishName: 'Leigh',
+        url: ""
+    },
+    {
+        id: 28,
+        slang: '高头大',
+        chineseName: "马",
+        description: "高头大马",
+        method: '补充',
+        englishName: 'Matthews',
+        url: ""
+    },
+    {
+        id: 29,
+        slang: '大滑子',
+        chineseName: "尤",
+        description: "大滑联想到油",
+        method: '联想',
+        englishName: 'Younis',
+        url: ""
+    },
+    {
+        id: 30,
+        slang: '大江大',
+        chineseName: "海",
+        description: "大江大海",
+        method: '补充',
+        englishName: 'Hathaway',
+        url: ""
+    },
+    {
+        id: 31,
+        slang: '捣米子',
+        chineseName: "褚",
+        description: "舂臼中之谷，乃以杵。",
+        method: '联想',
+        englishName: 'Christensen',
+        url: ""
+    },
+    {
+        id: 32,
+        slang: '梯子',
+        chineseName: "高",
+        description: "梯子用于登高。",
+        method: '联想',
+        englishName: 'Galloway',
+        url: ""
+    },
+    {
+        id: 33,
+        slang: '疙瘩',
+        chineseName: "记",
+        description: "以绳系物，结扣处俗称打“疙瘩”",
+        method: '联想',
+        englishName: 'Jirik',
+        url: ""
+    },
+    {
+        id: 34,
+        slang: '古月子',
+        chineseName: "褚",
+        description: "舂臼中之谷，乃以杵。",
+        method: '联想',
+        englishName: 'Christensen',
+        url: ""
+    },
+    {
+        id: 35,
+        slang: '锅烂',
+        chineseName: "周",
+        description: "烂了的锅只能凑和着用来熬粥",
+        method: '联想',
+        englishName: 'Zhagnay',
+        url: ""
+    },
+    {
+        id: 36,
+        slang: '横行子',
+        chineseName: "谢",
+        description: "螃蟹横行",
+        method: '联想',
+        englishName: 'Xanthos',
+        url: ""
+    },
+    {
+        id: 37,
+        slang: '九江八',
+        chineseName: "何",
+        description: "九江八河",
+        method: '补充',
+        englishName: 'Hewitt',
+        url: ""
+    },
+    {
+        id: 38,
+        slang: '喇叭',
+        chineseName: "崔",
+        description: "吹喇叭",
+        method: '联想',
+        englishName: 'Cunningham',
+        url: ""
+    },
+    {
+        id: 39,
+        slang: '辣子',
+        chineseName: "姜",
+        description: "姜味辣",
+        method: '联想',
+        englishName: 'Jiminez',
+        url: ""
+    },
+    {
+        id: 40,
+        slang: '两截子',
+        chineseName: "段",
+        description: "断成两截子",
+        method: '联想',
+        englishName: 'Dunbar',
+        url: ""
+    },
+    {
+        id: 41,
+        slang: '山根',
+        chineseName: "石",
+        description: "山根石",
+        method: '联想',
+        englishName: 'Showell',
+        url: ""
+    },
+    {
+        id: 42,
+        slang: '山中高',
+        chineseName: "塔",
+        description: "塔矗立山巅，为山中之高者",
+        method: '联想',
+        englishName: 'Talbot',
+        url: ""
+    },
+    {
+        id: 43,
+        slang: '双梢子',
+        chineseName: "林",
+        description: "“木”字中一竖耸立，故比之树“梢”。“林”字可析为双木，故以“双梢”指代姓氏“林”。",
+        method: '联想',
+        englishName: 'Lindsay',
+        url: ""
+    },
+    {
+        id: 44,
+        slang: '顺水',
+        chineseName: "刘",
+        description: "顺水流。",
+        method: '联想',
+        englishName: 'Lister',
+        url: ""
+    },
+    {
+        id: 45,
+        slang: '四方子',
+        chineseName: "郑",
+        description: "四方之为形，方方正正。",
+        method: '联想',
+        englishName: 'Zack',
+        url: ""
+    },
+    {
+        id: 46,
+        slang: '一点首',
+        chineseName: "方",
+        description: "万蔓同音，脑袋上多一点",
+        method: '拆字',
+        englishName: 'Fenton',
+        url: ""
+    },
+    {
+        id: 47,
+        slang: '白给',
+        chineseName: "宋",
+        description: "“白给”，义为“送”。",
+        method: '联想',
+        englishName: 'Sorensen',
+        url: ""
     },
 
 ];
