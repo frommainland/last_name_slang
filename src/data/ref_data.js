c0aa11874436c2a8556ca0dfd237ed0a9ce284c7
frommainland <caihehuang@gmail.com>
2022-11-09 17:27:22 +0800

    add - contentBanditRef function and content

---
+++ src/data/ref_data.js
@@ -0,0 +1,38 @@
+export const refs = [
+    {
+        id: 1,
+        href: "https://book.douban.com/subject/5290297/",
+        logo: 'title1',
+        icon: 'book',
+        iconName: '图书'
+    },
+    {
+        id: 2,
+        href: "https://book.douban.com/subject/2972398/",
+        logo: 'title2',
+        icon: 'book',
+        iconName: '图书'
+    },
+    {
+        id: 3,
+        href: "https://www.xiaoyuzhoufm.com/episode/633d37321aeb0012dbc352ce?s=eyJ1IjogIjYxN2ZhYTQ1ZTBmNWU3MjNiYmM5OGZjNyJ9",
+        logo: 'title3',
+        icon: 'headphones',
+        iconName: '播客'
+    },
+    {
+        id: 4,
+        href: "https://movie.douban.com/subject/2373267/",
+        logo: 'title4',
+        icon: 'tv',
+        iconName: '电视剧'
+    },
+    {
+        id: 5,
+        href: "https://music.douban.com/subject/6550584/",
+        logo: 'title5',
+        icon: 'headphones',
+        iconName: '评书'
+    },
+
+];
